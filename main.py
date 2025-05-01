3bb26c571b82b66b4892deeadf1e7f4b7199f9c7
OscarCodev <oussback46@gmail.com>
2025-05-01 12:59:56 -0500

    second change

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from typing import List, Optional
 from datetime import datetime
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker, Session
 import os
 from dotenv import load_dotenv
 
